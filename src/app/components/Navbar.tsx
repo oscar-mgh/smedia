7f6ccdcd7d3eef8f7ee7cb51d52b3f0905a849f5
Oscar <oscar.galindo1@yahoo.com>
2023-07-04 15:01:06 -0600

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="">
+        <img src="https://colorhunt.co/img/color-hunt-logo-face.svg" alt="" className="w-12 mt-8" />
+    </nav>
+  )
+}
